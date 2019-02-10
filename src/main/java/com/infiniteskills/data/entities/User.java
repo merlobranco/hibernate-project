9db0edf21bad0c95568e6df0bd0b687dd8a2a30b
Brais Cidras <braisomary@gmail.com>
2019-02-10 18:29:37 +0100

    Simple Entity Mapping

--- src/main/java/com/infiniteskills/data/entities/User.java
+++ src/main/java/com/infiniteskills/data/entities/User.java
@@ -2,24 +2,44 @@ package com.infiniteskills.data.entities;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="FINANCES_USER")
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="USER_ID")
 	private Long userId;
 	
+	@Column(name="FIRST_NAME")
 	private String firstName;
 	
+	@Column(name="LAST_NAME")
 	private String lastName;
 	
+	@Column(name="BIRTH_DATE")
 	private Date birthDate;
 	
+	@Column(name="EMAIL_ADDRESS")
 	private String emailAddress;
 	
-	private Date lastUpdadedDate;
+	@Column(name="LAST_UPDATED_DATE")
+	private Date lastUpdatedDate;
 	
+	@Column(name="LAST_UPDATED_BY")
 	private String lastUpdatedBy;
 	
+	@Column(name="CREATED_DATE")
 	private Date createdDate;
 	
+	@Column(name="CREATED_BY")
 	private String createdBy;
 
 	public Long getUserId() {
@@ -62,12 +82,12 @@ public class User {
 		this.emailAddress = emailAddress;
 	}
 
-	public Date getLastUpdadedDate() {
-		return lastUpdadedDate;
+	public Date getLastUpdatedDate() {
+		return lastUpdatedDate;
 	}
 
-	public void setLastUpdadedDate(Date lastUpdadedDate) {
-		this.lastUpdadedDate = lastUpdadedDate;
+	public void setLastUpdatedDate(Date lastUpdatedDate) {
+		this.lastUpdatedDate = lastUpdatedDate;
 	}
 
 	public String getLastUpdatedBy() {
