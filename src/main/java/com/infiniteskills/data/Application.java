b9d834ab7b3a27bdf37b2c3cda02a7b60952d9c3
Brais Cidras <braisomary@gmail.com>
2019-02-10 21:18:57 +0100

    Calling Persistence Methods example

--- src/main/java/com/infiniteskills/data/Application.java
+++ src/main/java/com/infiniteskills/data/Application.java
@@ -1,6 +1,10 @@
 package com.infiniteskills.data;
 
+import java.util.Date;
+
 import org.hibernate.Session;
+
+import com.infiniteskills.data.entities.User;
 
 /**
  * Class created for testing purposes
@@ -11,7 +15,22 @@ public class Application {
 	
 	public static void main(String[] args) {
 		Session session = HibernateUtil.getSessionfactory().openSession();
-		session.beginTransaction();
+		session.getTransaction().begin();
+		
+		User user = new User();
+		Date date = new Date();
+		user.setBirthDate(date);
+		user.setCreatedDate(date);
+		user.setCreatedBy("Brais");
+		user.setEmailAddress("brais@email.com");
+		user.setFirstName("Brais");
+		user.setLastName("Cidras");
+		user.setLastUpdatedBy("Brais");
+		user.setLastUpdatedDate(date);
+		
+		session.save(user);
+		session.getTransaction().commit();
 		session.close();
+
 	}
 }
