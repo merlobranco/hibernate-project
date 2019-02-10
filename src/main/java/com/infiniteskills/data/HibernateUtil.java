d0e773cec981ab61fc91142ec0dcd3a276dce9f5
Brais Cidras <braisomary@gmail.com>
2019-02-10 21:13:35 +0100

    Adding Hibernate Configuration file Hibernate 5.2.17.Final version support

--- src/main/java/com/infiniteskills/data/HibernateUtil.java
+++ src/main/java/com/infiniteskills/data/HibernateUtil.java
@@ -1,11 +1,14 @@
 package com.infiniteskills.data;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 	
+	private static StandardServiceRegistry registry;
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 
 	public static SessionFactory getSessionfactory() {
@@ -14,13 +17,20 @@ public class HibernateUtil {
 	
 	private static SessionFactory buildSessionFactory() {
 		try {
-			Configuration configuration = new Configuration();
-			return configuration.buildSessionFactory(new StandardServiceRegistryBuilder()
-														.applySettings(configuration.getProperties())
-														.build());
+			// Create registry
+			registry = new StandardServiceRegistryBuilder().configure().build();
+            // Create MetadataSources
+            MetadataSources sources = new MetadataSources(registry);
+            // Create Metadata
+            Metadata metadata = sources.getMetadataBuilder().build();
+            // Create SessionFactory
+            return metadata.getSessionFactoryBuilder().build();
 		}
 		catch(Exception e) {
 			e.printStackTrace();
+			if (registry != null) {
+                StandardServiceRegistryBuilder.destroy(registry);
+            }
 			throw new RuntimeException("There was an error building the factory");
 		}
 	}
