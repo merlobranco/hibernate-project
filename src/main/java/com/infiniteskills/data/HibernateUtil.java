10e7336c7d1d130c5a164d66079f440b91db3656
Brais Cidras <braisomary@gmail.com>
2019-02-10 20:53:44 +0100

    Adding Hibernate Configuration file support

--- src/main/java/com/infiniteskills/data/HibernateUtil.java
+++ src/main/java/com/infiniteskills/data/HibernateUtil.java
@@ -4,8 +4,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
-import com.infiniteskills.data.entities.User;
-
 public class HibernateUtil {
 	
 	private static final SessionFactory sessionFactory = buildSessionFactory();
@@ -17,8 +15,9 @@ public class HibernateUtil {
 	private static SessionFactory buildSessionFactory() {
 		try {
 			Configuration configuration = new Configuration();
-			configuration.addAnnotatedClass(User.class);
-			return configuration.buildSessionFactory(new StandardServiceRegistryBuilder().build());
+			return configuration.buildSessionFactory(new StandardServiceRegistryBuilder()
+														.applySettings(configuration.getProperties())
+														.build());
 		}
 		catch(Exception e) {
 			e.printStackTrace();
