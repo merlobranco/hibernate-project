f2d3743f8ebe2ca29e610c44559beebdbfd0ce49
Brais Cidras <braisomary@gmail.com>
2019-02-13 13:17:12 +0100

    Unidirectional One To One Association - Starting Point

---
+++ src/main/java/com/infiniteskills/data/entities/Credential.java
@@ -0,0 +1,49 @@
+package com.infiniteskills.data.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CREDENTIAL")
+public class Credential {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="CREDENTIAL_ID")
+	public Long credentialId;
+	
+	@Column(name="USERNAME")
+	private String username;
+
+	@Column(name="PASSWORD")
+	private String password;
+
+	public Long getCredentialId() {
+		return credentialId;
+	}
+
+	public void setCredentialId(Long credentialId) {
+		this.credentialId = credentialId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
