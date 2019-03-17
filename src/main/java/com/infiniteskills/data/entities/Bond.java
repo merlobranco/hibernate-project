09fb23216112c7d01e55cb06481f374575766ef6
Brais Cidras <braisomary@gmail.com>
2019-03-17 18:54:01 +0100

    Mapped Superclass Inheritance - Starting Point

---
+++ src/main/java/com/infiniteskills/data/entities/Bond.java
@@ -0,0 +1,98 @@
+package com.infiniteskills.data.entities;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "BOND")
+public class Bond {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "BOND_ID")
+	private Long bondId;
+	
+	@Column(name = "NAME")
+	protected String name;
+
+	@Column(name = "ISSUER")
+	protected String issuer;
+
+	@Column(name = "PURCHASE_DATE")
+	protected Date purchaseDate;
+
+	@Column(name = "VALUE")
+	private BigDecimal value;
+
+	@Column(name = "INTEREST_RATE")
+	private BigDecimal interestRate;
+
+	@Temporal(value = TemporalType.DATE)
+	@Column(name = "MATURITY_DATE")
+	private Date maturityDate;
+
+	public Long getBondId() {
+		return bondId;
+	}
+
+	public void setBondId(Long bondId) {
+		this.bondId = bondId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIssuer() {
+		return issuer;
+	}
+
+	public void setIssuer(String issuer) {
+		this.issuer = issuer;
+	}
+
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public BigDecimal getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(BigDecimal interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public Date getMaturityDate() {
+		return maturityDate;
+	}
+
+	public void setMaturityDate(Date maturityDate) {
+		this.maturityDate = maturityDate;
+	}
+
+}
---
+++ src/main/java/com/infiniteskills/data/entities/Stock.java
@@ -0,0 +1,84 @@
+package com.infiniteskills.data.entities;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "STOCK")
+public class Stock {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "STOCK_ID")
+	private Long stockId;
+	
+	@Column(name = "NAME")
+	protected String name;
+
+	@Column(name = "ISSUER")
+	protected String issuer;
+
+	@Column(name = "PURCHASE_DATE")
+	protected Date purchaseDate;
+
+	@Column(name = "SHARE_PRICE")
+	private BigDecimal sharePrice;
+
+	@Column(name = "QUANTITY")
+	private BigDecimal quantity;
+
+	public Long getStockId() {
+		return stockId;
+	}
+
+	public void setStockId(Long stockId) {
+		this.stockId = stockId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIssuer() {
+		return issuer;
+	}
+
+	public void setIssuer(String issuer) {
+		this.issuer = issuer;
+	}
+
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+	
+	public BigDecimal getSharePrice() {
+		return sharePrice;
+	}
+
+	public void setSharePrice(BigDecimal sharePrice) {
+		this.sharePrice = sharePrice;
+	}
+
+	public BigDecimal getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
+}
+
